10297399a26fb07c8a80aeddddb35e1bb76f2506
leRourou <axel.rouquette@outlook.fr>
2023-12-19 16:50:41 +0100

    Add list

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+MESURES_LIST = ['co2', 'humidity', 'uv', 'db', 'temperature']
+
 def parseArray(array):
     parsed_array = array.split(',')
     parsed_array = [value for value in parsed_array]
@@ -14,29 +16,35 @@ def error(message: str):
 
 @app.route('/data')
 def data():
-    required_args = ['start', 'end', 'interval', 'mesures', 'incomfort']
+    required_args = ['start', 'end', 'interval']
     for arg in required_args:
         if arg not in request.args:
             return error(f'Missing argument : {arg}')
-        
+    
     # Getting parameters
     tStart = int(request.args.get('start'))
     tEnd = int(request.args.get('end'))
     tInterval = int(request.args.get('interval'))
     mesures = request.args.get('mesures')
-    incomfort = request.args.get('incomfort')
-    parsed_mesures = parseArray(mesures)
-    parsed_incomfort = parseArray(incomfort)
+    discomfort = request.args.get('discomfort')
 
     # Verification
+    mesuresArgs = [mesures, discomfort]
+    for i in range(len(mesuresArgs)):
+        if mesuresArgs[i] is None:
+            mesuresArgs[i] = MESURES_LIST
+        else:
+            for m in mesuresArgs[i]:
+                if (m not in MESURES_LIST):
+                    return error(f"'{m}' is not a known mesure.")
     if (tStart < 0): return error("Start timestamp can't be negative")
     if (tEnd < 0): return error("End timestamp can't be negative")
     if (tInterval < 0): return error("End timestamp can't be negative")
     if (tStart > tEnd): return error("Start timestamp can't be superior to end timestamp")
 
     return jsonify({
-        'Mesure filters': parsed_mesures,
-        'Incomfort filters': parsed_incomfort,
+        'Mesure filters': mesures,
+        'Incomfort filters': discomfort,
         'Timestamp start': tStart,
         'Timestamp end': tEnd,
         'Interval': f'{tInterval} minutes'
