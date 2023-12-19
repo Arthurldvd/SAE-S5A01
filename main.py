5d1e85fda7db8c494d7a5d6922cbbb368a46ba14
leRourou <axel.rouquette@outlook.fr>
2023-12-19 15:22:36 +0100

    Add filters and good request

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from data_types import DataTypes
 
 app = Flask(__name__)
 
-@app.route('/data/<type>')
-def data(type: DataTypes):
+@app.route('/data')
+def data():
     tStart = request.args.get('start')
     tEnd = request.args.get('end')
-    if (tStart in [None, ""]): 
-      return "You must specifiy a start date."
-    if (tEnd in [None, ""]):
-       return "You must specify a end date."
-    if (tStart > tEnd):
-       return "Start date is superior to end date."
-    
-    return f'{type}, {tStart}, {tEnd}'
+    tInterval = request.args.get('interval')
+    mesures = request.args.get('mesures')
+    incomfort = request.args.get('incomfort')
+    parsed_mesures = parseArray(mesures)
+    parsed_incomfort = parseArray(incomfort)
+    return f"Mesure filters: {parsed_mesures},\n Incomfort filters: {parsed_incomfort} TimTimestamp start: {tStart},\n Timestamp end: {tEnd}, Intervale: {tInterval} minutes"
 
+def parseArray(array):
+    parsed_array = array.split(',')
+    parsed_array = [value for value in parsed_array]
+    return parsed_array
