d765df696bb634659e004dfba0740df658758490
XXGIGASHADOWPAUL38XX <108007727+XXGIGASHADOWPAUL38XX@users.noreply.github.com>
2023-12-19 11:00:53 +0100

    avancée

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from influxdb_client import InfluxDBClient
+from influxdb_service import init_influxdb, request_influxBD
 
-client = InfluxDBClient(
-    url = '51.83.36.122:8086',
-    token = 'q4jqYhdgRHuhGwldILZ2Ek1WzGPhyctQ3UgvOII-bcjEkxqqrIIacgePte33CEjekqsymMqWlXnO0ndRhLx19g==',
-    org = 'INFO',
-    bucket = 'iut_bucket'
-    )
+init_influxdb()
+data = request_influxBD(
+    '''from(bucket: "IUT_BUCKET")
+      |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
+      |> filter(fn: (r) => r["_measurement"] == "%")
+      |> filter(fn: (r) => r["_field"] == "value")
+      |> filter(fn: (r) => r["domain"] == "sensor")
+      |> filter(fn: (r) => r["entity_id"] == "d251_1_co2_humidity" or r["entity_id"] == "d251_1_multisensor_humidity" or r["entity_id"] == "d351_1_co2_humidity" or r["entity_id"] == "d351_1_multisensor9_humidity" or r["entity_id"] == "d351_1_multisensor9_smoke_density" or r["entity_id"] == "d351_1_multisensor_humidity" or r["entity_id"] == "d351_2_multisensor_humidity" or r["entity_id"] == "d351_2_co2_humidity" or r["entity_id"] == "d351_3_co2_humidity" or r["entity_id"] == "d360_1_co2_humidity" or r["entity_id"] == "d360_1_multisensor_humidity_2")
+      |> aggregateWindow(every: v.windowPeriod, fn: mean, createEmpty: false)
+      |> yield(name: "mean")'''
+)
 
-query_api = client.query_api()
 tables = query_api.query('from(bucket: "IUT_BUCKET") |> range(start: -1h)')
+
+results = []
+for table in tables:
+  for record in table.records:
+    results.append((record.get_field(), record.get_value()))
+
+print(results)
