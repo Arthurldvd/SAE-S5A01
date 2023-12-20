6e9dbb2f3803eb09e38882371823a670f1beea6c
leRourou <axel.rouquette@outlook.fr>
2023-12-20 10:00:47 +0100

    Change to measures

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 from functools import reduce
-
-from flask import Flask, request, jsonify
 import re
+from flask import Flask, request, jsonify
 
 from incomfort_constraints import check_constraint
 from influxdb_service import init_influxdb, request_influxBD
 
 app = Flask(__name__)
 
-MESURES_LIST = ['%', 'dBA', 'ppm', '°C', 'µg\/m³']
+MEASURES_LIST = ['%', 'dBA', 'ppm', '°C', 'µg\/m³']
 DISCOMFORT_LIST = ['co2', 'humidity', 'uv', 'db', 'temperature']
 
 def parseArray(array):
-    return array.split(',')
+    if array is None: return None
+    return [value for value in array.split(',')]
 
 def error(message: str):
-    return {
-        'error' : message
-    }
-
-def addition(x, y):
-    for i in range(y):
-        x = x + 1
+    return jsonify({
+        'error': message
+    }), 400
 
 @app.route('/data')
 def data():
@@ -34,29 +30,33 @@ def data():
     tStart = int(request.args.get('start'))
     tEnd = int(request.args.get('end'))
     tInterval = (request.args.get('interval'))
-    mesures = parseArray(request.args.get('mesures')) if request.args.get('salle') is not None else MESURES_LIST
-    discomfort = parseArray(request.args.get('discomfort')) if request.args.get('discomfort') is not None else DISCOMFORT_LIST
+    measures = parseArray(request.args.get('measures'))
+    discomfort = parseArray(request.args.get('discomfort'))
     salle = request.args.get('salle') if request.args.get('salle') is not None else ""
 
     # Verification
-    for m in mesures:
-        if (m not in MESURES_LIST):
-            return error(f"'{m}' is not a known mesure.")
+    if (measures is None): measures = MEASURES_LIST
+    else:
+        for m in measures:
+            if (m not in MEASURES_LIST):
+                return error(f"'{m}' is not a known measure.")
 
-    for m in discomfort:
-        if (m not in DISCOMFORT_LIST):
-            return error(f"'{m}' is not a known discomfort.")
+    if (discomfort is None): discomfort = DISCOMFORT_LIST
+    else:
+        for m in discomfort:
+            if (m not in DISCOMFORT_LIST):
+                return error(f"'{m}' is not a known discomfort.")
 
-    if (tStart < 0): return error("Start timestamp can't be negative")
-    if (tEnd < 0): return error("End timestamp can't be negative")
-    if (tStart > tEnd): return error("Start timestamp can't be superior to end timestamp")
-    if not (regex_match(str(tInterval), r'^[1-9]+\d*(m|h|d|w|mo|y])$')): return error("Interval is not in a correct format")
+    if (tStart < 0): return error("Start timestamp can't be negative.")
+    if (tEnd < 0): return error("End timestamp can't be negative.")
+    if (tStart > tEnd): return error("Start timestamp can't be superior to end timestamp.")
+    if not (regex_match(str(tInterval), r'^[1-9]+\d*(m|h|d|w|mo|y])$')): return error("Interval is not in a correct format.")
 
-    return filter_data(tStart, tEnd, tInterval, convert_regex(mesures), discomfort, salle)
+    return filter_data(tStart, tEnd, tInterval, convert_regex(measures), discomfort, salle)
 
 if __name__ == '__main__':
     app.run(debug=True)
-def filter_data(tStart, tEnd, tInterval, mesures, incomfort, salle):
+def filter_data(tStart, tEnd, tInterval, measures, incomfort, salle):
     init_influxdb()
     request = f'''
     import "strings"
@@ -64,14 +64,13 @@ def filter_data(tStart, tEnd, tInterval, mesures, incomfort, salle):
     
     from(bucket: "IUT_BUCKET")
           |> range(start: {tStart}, stop: {tEnd})
-          |> filter(fn: (r) => r["_measurement"] =~ {mesures})
+          |> filter(fn: (r) => r["_measurement"] =~ {measures})
           |> filter(fn: (r) => r["_field"] == "value")
           |> filter(fn: (r) => r["domain"] == "sensor")
           |> filter(fn: (r) => strings.hasPrefix(v: r["entity_id"], prefix: "{salle}"))
           |> aggregateWindow(every: {tInterval}, fn: mean, createEmpty: false)
           |> yield(name: "mean")
         '''
-    print(request)
     data = request_influxBD(request)
     data_after_constraints = check_constraint(data, incomfort)
     return data_after_constraints
