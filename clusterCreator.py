2f6ddf22cf9f7c3b7b23394582c772a5e6490053
XXGIGASHADOWPAUL38XX <108007727+XXGIGASHADOWPAUL38XX@users.noreply.github.com>
2024-02-14 09:18:58 +0100

    unités pour rourou

--- clusterCreator.py
+++ clusterCreator.py
@@ -10,14 +10,14 @@ def get_mesure(record):
     return mesure_name
 
 mesures = [
-    Mesure('Mesure d\'humidié dans l\'air en %', lambda record: record._measurement == '%' and record.entity_id != 'd351_1_multisensor9_smoke_density'),
-    Mesure('Mesure de fumée dans l\'air en %', lambda record: record._measurement == '%' and record.entity_id == 'd351_1_multisensor9_smoke_density'),
+    Mesure('Humidité', lambda record: record._measurement == '%' and record.entity_id != 'd351_1_multisensor9_smoke_density'),
+    Mesure('Fumée', lambda record: record._measurement == '%' and record.entity_id == 'd351_1_multisensor9_smoke_density'),
     Mesure(lambda record: record._measurement, lambda record: record._measurement.startswith('binary_sensor')),
-    Mesure('Mesure de décibels', lambda record: record._measurement == 'dBA'),
-    Mesure('Mesure de luminosité en lx', lambda record: record._measurement == 'lx'),
-    Mesure('Mesure de co2 en ppm', lambda record: record._measurement == 'ppm' and record.entity_id.endswith('co2_level')),
-    Mesure('Mesure de co2 volatil en ppm', lambda record: record._measurement == 'ppm' and record.entity_id.endswith('organic_compound_level')),
-    Mesure('Mesure de la température de l\'air', lambda record: record._measurement == '°C' and (lambda record: record.entity_id.endswith('air_temperature') or record.entity_id.endswith('air_temperature_2'))),
-    Mesure('co2 dew point ?', lambda record: record._measurement == '°C' and record.entity_id.endswith('co2_dew_point')),
-    Mesure('Mesure du taux de particules', lambda record: record._measurement == 'µg/m³')
+    Mesure('Décibels', lambda record: record._measurement == 'dBA'),
+    Mesure('Luminosité', lambda record: record._measurement == 'lx'),
+    Mesure('Co2', lambda record: record._measurement == 'ppm' and record.entity_id.endswith('co2_level')),
+    Mesure('Co2 Volatil', lambda record: record._measurement == 'ppm' and record.entity_id.endswith('organic_compound_level')),
+    Mesure('Température', lambda record: record._measurement == '°C' and (lambda record: record.entity_id.endswith('air_temperature') or record.entity_id.endswith('air_temperature_2'))),
+    Mesure('co2 Dew', lambda record: record._measurement == '°C' and record.entity_id.endswith('co2_dew_point')),
+    Mesure('Particules', lambda record: record._measurement == 'µg/m³')
 ]
